12ea144f95398f923c609a8c173fbcb90cc34339
Dan Garrette <dhgarrette@gmail.com>
2012-08-28 01:36:40 -0500

    sts stuff

--- src/main/scala/utcompling/mlnsemantics/run/RunFull.scala
+++ src/main/scala/utcompling/mlnsemantics/run/RunFull.scala
@@ -9,9 +9,7 @@ import utcompling.scalalogic.fol.expression.FolExpression
 import utcompling.mlnsemantics.modal.ModalDiscourseInterpreter
 import utcompling.mlnsemantics.wordnet.WordnetImpl
 import utcompling.mlnsemantics.vecspace.BowVectorSpace
-import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.impl.MergingBoxerExpressionInterpreterDecorator
-import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.impl.UnnecessarySubboxRemovingBoxerExpressionInterpreter
-import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.impl.OccurrenceMarkingBoxerExpressionInterpreterDecorator
+import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.impl._
 import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.BoxerExpressionInterpreter
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -20,56 +18,3 @@ import utcompling.scalalogic.discourse.candc.boxer.expression.interpreter.impl.P
 import utcompling.mlnsemantics.inference._
 import utcompling.mlnsemantics.datagen.Tokenize
 
-class RunFull(
-  vecspaceFactory: (String => Boolean) => Map[String, BowVector]) {
-
-  def run(txt: String, hyp: String): Unit = {
-    run(Tokenize(txt).toList, Tokenize(hyp).toList)
-  }
-
-  def run(txt: List[String], hyp: List[String]): Unit = {
-    val ttp =
-      new TextualTheoremProver(
-        new ModalDiscourseInterpreter(),
-        new InferenceRuleInjectingProbabilisticTheoremProver(
-          new WordnetImpl(),
-          vecspaceFactory,
-          new TopRuleWeighter(
-            new RankingRuleWeighter(
-              new VecspaceRuleWeighter(
-                new SimpleCompositeVectorMaker()))),
-          new TypeConvertingPTP(
-            new BoxerExpressionInterpreter[FolExpression] {
-              def interpret(x: BoxerExpression): FolExpression =
-                new Boxer2DrtExpressionInterpreter().interpret(
-                  new OccurrenceMarkingBoxerExpressionInterpreterDecorator().interpret(
-                    new MergingBoxerExpressionInterpreterDecorator().interpret(
-                      new UnnecessarySubboxRemovingBoxerExpressionInterpreter().interpret(
-                        new PredicateCleaningBoxerExpressionInterpreterDecorator().interpret(x))))).fol
-            },
-            new AlchemyTheoremProver(FileUtils.pathjoin(System.getenv("HOME"), "bin/alchemy/bin/infer")))))
-
-    println(txt.mkString(" "))
-    println(hyp.mkString(" "))
-    println(ttp.prove(txt, hyp))
-
-    //    def mtpo = new ModalTheoremProver(tpo)
-    //    def vtpo = new VisualizingModalTheoremProverDecorator(mtpo)
-    //    def vwtpo = new VisualizingModalTheoremProverDecorator(new InferenceRuleInjectingProbabilisticTheoremProver(mtpo))
-  }
-
-}
-
-object RunFull {
-
-  def main(args: Array[String]) {
-    Logger.getRootLogger.setLevel(Level.INFO)
-
-    val vsf2 = (words: (String => Boolean)) => BowVectorSpace("resources/nytgiga.lem.1m.vc.f2000.m50.wInf", words)
-
-    val runner = new RunFull(vsf2)
-    runner.run("A man is riding a bicycle.", "A man is riding a bike.")
-
-  }
-
-}
--- src/main/scala/utcompling/mlnsemantics/run/Sts.scala
+++ src/main/scala/utcompling/mlnsemantics/run/Sts.scala
@@ -23,6 +23,13 @@ import utcompling.mlnsemantics.datagen.Tokenize
 import utcompling.mlnsemantics.datagen.CncLemmatizeCorpusMapper
 import scala.io.Source
 
+/**
+ *
+ *
+ * sbt "run-main utcompling.mlnsemantics.run.Sts lem resources/semantic-textual-similarity/STS.input.MSRvid.txt resources/semantic-textual-similarity/STS.input.MSRvid.lem""
+ * sbt "run-main utcompling.mlnsemantics.run.Sts vs resources/full.vs resources/semantic-textual-similarity/STS.input.MSRvid.lem resources/semantic-textual-similarity/STS.input.MSRvid.vs"
+ * sbt "run-main utcompling.mlnsemantics.run.Sts fromStsVs resources/semantic-textual-similarity/STS.input.MSRvid.txt resources/semantic-textual-similarity/STS.input.MSRvid.vs"
+ */
 object Sts {
 
   def main(args: Array[String]) {
@@ -39,13 +46,52 @@ object Sts {
         }
 
       case Seq("vs", fullVsFile, lemFile, stsVsFile) =>
-        
-        
-      case Seq("do") =>
-        val vsf2 = (words: (String => Boolean)) => BowVectorSpace("resources/nytgiga.lem.1m.vc.f2000.m50.wInf", words)
-        val runner = new RunFull(vsf2)
-        runner.run("A man is riding a bicycle.", "A man is riding a bike.")
+        val allLemmas = Source.fromFile(fullVsFile).getLines.flatMap(_.split("\\s+")).toSet
+        FileUtils.writeUsing(stsVsFile) { f =>
+          for (line <- Source.fromFile(fullVsFile).getLines)
+            if (allLemmas(line.split("\\s+")(0)))
+              f.write(line + "\n")
+        }
 
+      case Seq("full", stsFile, fullVsFile) =>
+        val sentences = Source.fromFile(stsFile).getLines.flatMap(_.split("\t")).toVector
+        val lemmatized = new CncLemmatizeCorpusMapper().parseToLemmas(sentences)
+        val allLemmas = lemmatized.flatten.flatMap(_.map(_._2)).toSet
+        run(stsFile, fullVsFile, _ => true)
+
+      case Seq("fromStsVs", stsFile, stsVsFile) =>
+        run(stsFile, stsVsFile, UniversalSet())
+    }
+
+    def run(stsFile: String, vsFile: String, allLemmas: String => Boolean) {
+      val ttp =
+        new TextualTheoremProver(
+          new ModalDiscourseInterpreter(),
+          new InferenceRuleInjectingProbabilisticTheoremProver(
+            new WordnetImpl(),
+            BowVectorSpace(vsFile, allLemmas).filterKeys,
+            new TopRuleWeighter(
+              new RankingRuleWeighter(
+                new VecspaceRuleWeighter(
+                  new SimpleCompositeVectorMaker()))),
+            new TypeConvertingPTP(
+              new BoxerExpressionInterpreter[FolExpression] {
+                def interpret(x: BoxerExpression): FolExpression =
+                  new Boxer2DrtExpressionInterpreter().interpret(
+                    new OccurrenceMarkingBoxerExpressionInterpreterDecorator().interpret(
+                      new MergingBoxerExpressionInterpreterDecorator().interpret(
+                        new UnnecessarySubboxRemovingBoxerExpressionInterpreter().interpret(
+                          new PredicateCleaningBoxerExpressionInterpreterDecorator().interpret(x))))).fol
+              },
+              new AlchemyTheoremProver(FileUtils.pathjoin(System.getenv("HOME"), "bin/alchemy/bin/infer")))))
+
+      val pairs = Source.fromFile(stsFile).getLines.map(_.split("\t")).map { case Array(a, b) => (a, b) }.toVector
+
+      for ((txt, hyp) <- pairs) {
+        println(txt.mkString(" "))
+        println(hyp.mkString(" "))
+        println(ttp.prove(txt, hyp))
+      }
     }
   }
 
