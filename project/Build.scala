4c5c346bb7c7437ea9e6356f045fbd62b99a26c1
Dan Garrette <dhgarrette@gmail.com>
2012-08-30 03:01:40 -0500

    removed aggregation of subprojects b/c it breaks 'run'

--- project/Build.scala
+++ project/Build.scala
@@ -3,9 +3,9 @@ import Keys._
 
 object MlnSemanticsBuild extends Build {
 
-  lazy val main = Project("mln-semantics", file(".")) aggregate(scalaLogic) dependsOn(scalaLogic)
+  lazy val main = Project("mln-semantics", file(".")) dependsOn(scalaLogic)
 
-  lazy val scalaLogic = Project("scala-logic", file("scala-logic")) aggregate(scalabha) dependsOn(scalabha)
+  lazy val scalaLogic = Project("scala-logic", file("scala-logic")) dependsOn(scalabha)
 
   lazy val scalabha = Project("Scalabha", file("scala-logic/scalabha"))
 
