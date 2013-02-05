87131b2d4d6391ed1be4611882b98f7242c23a2b
beltagy <beltagy@beltagy.(none)>
2013-02-04 21:11:56 -0600

    fix bug related to eq exp with recovering Event

--- src/main/scala/utcompling/mlnsemantics/inference/PositiveEqEliminatingProbabilisticTheoremProver.scala
+++ src/main/scala/utcompling/mlnsemantics/inference/PositiveEqEliminatingProbabilisticTheoremProver.scala
@@ -29,18 +29,78 @@ class PositiveEqEliminatingProbabilisticTheoremProver(
     assumptions: List[WeightedExpression[FolExpression]],
     goal: FolExpression): Option[Double] = {
 
-    val equalities = findEq(goal);
-    val newAssumption = removeEq(assumptions.head.expression, equalities);
+    val goalEqualities = findEq(goal);
+    val newGoal = removeEq(goal, goalEqualities);
     
-    val newGoal = removeEq(goal, equalities);
+    val assumEqualities = findEq(assumptions.head.expression);
+    val newAssumption = removeEq(assumptions.head.expression, assumEqualities);
    
     val newAssumptions = List (HardWeightedExpression (newAssumption)) ++ 
     					assumptions.filterNot( _ == assumptions.head);
     
-    val newDeclarations = for (declaration <- declarations)
+    /*val newDeclarations = for (declaration <- declarations)
     					yield {
-      
+    	var declarationTypes = declaration._2;
+    	var declarationFol = declaration._1 match {
+    	  case FolAtom(pred, args @ _*) => {
+     		 
+     		 val equalities = pred.name.charAt(pred.name.length()-1) match {
+     		   case 'h' => goalEqualities;
+     		   case 't' => assumEqualities;
+     		 }
+     		 val argsCount = args.length;
+     		 val arg = declaration._1.asInstanceOf[FolApplicationExpression].argument;
+     		 val fun = declaration._1.asInstanceOf[FolApplicationExpression].function;
+    	     var newArg = arg; 
+	         for (p <- equalities)
+		     {
+		    	val origVarName = arg.asInstanceOf[FolVariableExpression].variable.name;
+    	    	if (p._2.equals(origVarName))
+		    	{
+		    	  newArg = FolVariableExpression(Variable(p._1));
+	    	     //set type
+		    	  val typ = p._1.charAt(0) match {
+			      	case 'x' => "indv"
+			        case 'e' => "evnt"
+			        case 'p' => "prop"
+			        case _ => "indv"
+		    	  }
+		    	  declarationTypes = declarationTypes.updated(argsCount-1, typ)
+		    	}
+		     }
+	         
+	    	 var newFun = fun match {
+	    	      case FolVariableExpression(v) => FolVariableExpression(v);
+	    	      case FolApplicationExpression(fun, arg) =>{
+	    	        var newArg = arg;
+	    	        for (p <- equalities)
+				    {
+				    	val origVarName = arg.asInstanceOf[FolVariableExpression].variable.name;
+		    	    	if (p._2.equals(origVarName))
+				    	{
+				    	  newArg = FolVariableExpression(Variable(p._1));
+			    	     //set type
+				    	  val typ = p._1.charAt(0) match {
+					      	case 'x' => "indv"
+					        case 'e' => "evnt"
+					        case 'p' => "prop"
+					        case _ => "indv"
+				    	  }
+				    	  declarationTypes = declarationTypes.updated(0, typ)
+				    	}
+				    }	    	        
+				    FolApplicationExpression (fun, newArg);
+	    	      };
+	    	      case _ => throw new Exception("wrong format of a declaration predicate");
+	    	 }
+	    	 FolApplicationExpression(newFun, newArg)
+
+    	  }
+    	  case _ => throw new Exception("wrong format of a declaration predicate");
+    	}
+    	declarationFol -> declarationTypes;
     }
+    */
     delegate.prove(
       constants,
       declarations,
@@ -51,7 +111,7 @@ class PositiveEqEliminatingProbabilisticTheoremProver(
   
   private def removeEq(input: FolExpression, eq: List[(String, String)]): FolExpression =
   {
-    input match {
+    input match {	
    	case FolExistsExpression(variable, term) => FolExistsExpression (removeEq(variable, eq), removeEq(term, eq));
 
    	//This is very important: Changing all IMP to AND because alchamy is very slow on processing IMP
@@ -100,14 +160,22 @@ class PositiveEqEliminatingProbabilisticTheoremProver(
    	}
    	
    	case FolEqualityExpression(first, second) => {
-   	  List((first match {
+   	  val var1 = first match {
    	    case FolVariableExpression(variable) => variable.name
    	    case _ => throw new Exception ("Unrecongnized format, argumets of equality expression should be variables");
-   	  },
-   	  second match {
+   	  }
+   	 
+   	  val var2 = second match {
    	    case FolVariableExpression(variable) => variable.name
    	    case _ => throw new Exception ("Unrecongnized format, argumets of equality expression should be variables");
-   	  }))
+   	  }
+   	  
+   	  //Both variables should be of the same type (both individuals or both events). 
+   	  //Cases where variables types are different imply that the sentence is not parsed correctly. 
+   	  //Such equality constraints are skipped because they are wrong and because they miss us the MLN 
+   	  if (var1.charAt(0) == var2.charAt(0))
+   	    return List((var1, var2));
+   	  else return List();
    	}
 
    	//case FolAtom(pred, args @ _*) => FolAtom(pred, args.map(v => Variable(namePrefix+v.name)))
