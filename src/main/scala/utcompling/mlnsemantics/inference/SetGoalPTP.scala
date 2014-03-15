38fabd0e4182718b7e0a6554df034d5ef6cca6fa
Islam Beltagy <beltagy@cs.utexas.edu>
2014-03-15 00:15:18 -0500

    wrong but works

--- src/main/scala/utcompling/mlnsemantics/inference/SetGoalPTP.scala
+++ src/main/scala/utcompling/mlnsemantics/inference/SetGoalPTP.scala
@@ -111,7 +111,8 @@ class SetGoalPTP(
       }
       extraExpressions = List(GoalExpression(expr.asInstanceOf[FolExpression], Double.PositiveInfinity));
       goalExist match {
-        case Some(g) => extraExpressions = HardWeightedExpression(g) :: extraExpressions; 
+//        case Some(g) => extraExpressions = HardWeightedExpression(g) :: extraExpressions; 
+        case Some(g) => extraExpressions = extraExpressions :+  HardWeightedExpression(g);
         case _ =>
       }
       //----------------------
@@ -127,7 +128,8 @@ class SetGoalPTP(
 	    	  				else 
 	    	  					introduction(e, false, null);
 		      textExit match {
-		        case Some(t) => extraExpressions = HardWeightedExpression(t) :: extraExpressions; true; 
+//		        case Some(t) => extraExpressions = HardWeightedExpression(t) :: extraExpressions; true; 
+              case Some(t) => extraExpressions = extraExpressions :+ HardWeightedExpression(t); true;
 		        case _ => false;
 		      }
           }
@@ -163,7 +165,7 @@ class SetGoalPTP(
     else
       throw new RuntimeException("Not possible to reach this point")
 
-    val res = delegate.prove(newConstants, declarations, evidence, extraExpressions ++ assumptions, null)
+    val res = delegate.prove(newConstants, declarations, evidence, assumptions ++ extraExpressions, null)
     if (negatedGoal)
     {
       require(res.size == 1);
