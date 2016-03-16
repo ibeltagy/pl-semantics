2b2cfa8362237894df743db2401e6c3248e2f881
Islam Beltagy <bell@wireless-10-147-152-121.public.utexas.edu>
2016-03-15 23:24:19 -0500

    candc.sh

--- candc/bin/candc.sh
+++ candc/bin/candc.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-candc/bin/pos --model candc/models/pos | candc/bin/ner --model candc/models/ner --ifmt "%w|%p \n" --ofmt "%w|%p|%n \n" |  candc/bin/chunk --model candc/models/chunk --ifmt "%w|%p|%n \n" --ofmt "%w|%p|%n|%c \n"  | candc/bin/parser --model candc/models/parser/ --super candc/models/super --ifmt "%w|%p|%n|%c \n" --pos candc/models/pos  -printer boxer
+#candc/bin/pos --model candc/models/pos | candc/bin/ner --model candc/models/ner --ifmt "%w|%p \n" --ofmt "%w|%p|%n \n" |  candc/bin/chunk --model candc/models/chunk --ifmt "%w|%p|%n \n" --ofmt "%w|%p|%n|%c \n"  | candc/bin/parser --model candc/models/parser/ --super candc/models/super --ifmt "%w|%p|%n|%c \n" --pos candc/models/pos  -printer boxer
+
+candc/bin/pos --model candc/models/pos --ifmt "%w|%n \n" --ofmt "%w|%p|%n \n" |  candc/bin/chunk --model candc/models/chunk --ifmt "%w|%p|%n \n" --ofmt "%w|%p|%n|%c \n"  | candc/bin/parser --model candc/models/parser/ --super candc/models/super --ifmt "%w|%p|%n|%c \n" --pos candc/models/pos  -printer boxer
+
